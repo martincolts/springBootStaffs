d484bd5c536fe6e7da8255910227d4ff2fe57fee
jmlopez-technisys <jmlopez@technisys.com>
2017-05-18 16:57:45 -0300

    configuracion de la base H2

---
+++ src/main/java/com/probando/algo/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.probando.algo;
+
+
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jmlopez on 18/05/17.
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
